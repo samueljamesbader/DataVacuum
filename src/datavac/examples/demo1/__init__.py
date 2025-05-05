3534d521e7f69d15af9c23d281c78527235b0126
Bader, Samuel <samuel.bader@intel.com>
2025-05-04 19:31:59 -0700

    re-run data

--- src/datavac/examples/demo1/__init__.py
+++ src/datavac/examples/demo1/__init__.py
@@ -12,6 +12,7 @@ os.environ['DATAVACUUM_READ_DIR']=str(READ_DIR)
 os.environ['DATAVACUUM_LAYOUT_PARAMS_DIR']=str(Path(__file__).parent/"config/layout_params")
 
 # Set the environment variables for a local test database
-os.environ['DATAVACUUM_DBSTRING']=f"Server = localhost; Port = 5432; Database = datavacuum_test;" \
+dbname='datavacuum_demo1'
+os.environ['DATAVACUUM_DBSTRING']=f"Server = localhost; Port = 5432; Database = {dbname};" \
                                   f" Uid = postgres; Password = {os.environ.get('DATAVACUUM_TEST_DB_PASS','')}"
 os.environ['DATAVACUUM_DB_DRIVERNAME']="postgresql"
